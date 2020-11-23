19551f1b8024e4bdec5ddda8a07a475a840be88d
Techdoodle <chris@techchrism.me>
2020-11-23 15:03:56 -0800

    make villagers approach less aggressively

--- src/main/java/me/techchrism/villageremeraldpathfinding/VillagerEmeraldPathfinding.java
+++ src/main/java/me/techchrism/villageremeraldpathfinding/VillagerEmeraldPathfinding.java
@@ -24,7 +24,7 @@ public class VillagerEmeraldPathfinding extends JavaPlugin
                     {
                         if(entity instanceof Villager)
                         {
-                            ((Villager) entity).getPathfinder().moveTo(player);
+                            ((Villager) entity).getPathfinder().moveTo(player, 0.65);
                         }
                     }
                 }
